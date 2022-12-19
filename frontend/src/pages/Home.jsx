6661a6cc0dffd3b64849cb9aa82eb8c63d512aef
o6ris <loic.tsiry@gmail.com>
2022-12-19 10:54:25 +0100

    fetch data ok. All numbers can be displayed with a toggle button

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,20 @@
-// import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Home() {
+  const [numbers, setNumbers] = useState([]);
+  const [isListDisplayed, setIsListDisplayed] = useState(false);
+  const getAllNumbers = () => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}/numbers`)
+      .then((nbrs) => setNumbers(nbrs.data))
+      .catch((error) => console.error(error));
+  };
+
+  useEffect(() => {
+    getAllNumbers();
+  }, []);
+
   return (
     <div className="flex flex-col items-center w-full gap-10">
       <h1 className="text-2xl text-gray-50 mt-5">Enter a number</h1>
@@ -16,15 +30,22 @@ export default function Home() {
         </label>
       </form>
       <div>
-        <button className="border p-2 bg-gray-50" type="button">
+        <button
+          onClick={() => setIsListDisplayed(!isListDisplayed)}
+          className="border p-2 bg-gray-50"
+          type="button"
+        >
           Show list of Numbers
         </button>
-        <ul className="flex flex-col w-full items-center gap-3 mt-5">
-          <li className="text-lg text-gray-50">number : bonjour</li>
-          <li>bonjour</li>
-          <li>bonjour</li>
-          <li>bonjour</li>
-        </ul>
+        {isListDisplayed && (
+          <ul className="flex flex-col w-full items-center gap-3 mt-5">
+            {numbers.map((number) => (
+              <li className="text-lg text-gray-50">
+                number : {number.myNumber}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
